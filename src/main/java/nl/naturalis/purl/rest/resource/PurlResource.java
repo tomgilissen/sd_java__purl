5554da02b7374c06e7af622073d82c820b532003
Ayco Holleman <ayco.holleman@naturalis.nl>
2015-06-08 15:38:36 +0200

    Basic PURL resource (responds to PURL pattern) works

--- src/main/java/nl/naturalis/purl/rest/resource/PurlResource.java
+++ src/main/java/nl/naturalis/purl/rest/resource/PurlResource.java
@@ -2,22 +2,25 @@ package nl.naturalis.purl.rest.resource;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 
 import org.domainobject.util.debug.BeanPrinter;
 
-@Path("/(naturalis|voff|floron)")
+@Path("/")
 public class PurlResource {
 
 	@GET
-	@Path("/{objecttype}/{id}")
-	public Response handle(UriInfo request)
+	@Path("/{institute: (naturalis|floron|voff)}/{objecttype}/{id}")
+	@Produces("text/plain;charset=UTF-8")
+	public String handle(@Context UriInfo request)
 	{
 		MultivaluedMap<String,String> params = request.getPathParameters();
 		BeanPrinter.out(params);
-		return null;
+		return request.getBaseUri().toString();
 	}
 
 }
