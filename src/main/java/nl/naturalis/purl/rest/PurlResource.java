192d30fdb332ab53636cd78fb884aa4bfcf7fc58
Ayco Holleman <ayco.holleman@naturalis.nl>
2020-03-25 10:32:45 +0100

    added version info to landing page.

--- src/main/java/nl/naturalis/purl/rest/PurlResource.java
+++ src/main/java/nl/naturalis/purl/rest/PurlResource.java
@@ -9,19 +9,18 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
-
+import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
 import nl.naturalis.nba.utils.StringUtil;
+import nl.naturalis.purl.BuildInfo;
 import nl.naturalis.purl.Registry;
 import nl.naturalis.purl.naturalis.NaturalisPurlHandler;
 import nl.naturalis.purl.waarneming.WaarnemingPurlHandler;
 import nl.naturalis.purl.xenocanto.XenoCantoPurlHandler;
 
 /**
- * Central class of the PURL service. Provides endpoints for PURLs, but
- * delegates all the heavy lifting to specialized {@link PurlHandler}s.
+ * Central class of the PURL service. Provides endpoints for PURLs, but delegates all the heavy lifting to specialized {@link PurlHandler}s.
  * 
  * @author Ayco Holleman
  *
@@ -29,68 +28,73 @@ import nl.naturalis.purl.xenocanto.XenoCantoPurlHandler;
 @Path("/")
 public class PurlResource {
 
-	@SuppressWarnings("unused")
-	private static final Logger logger = LogManager.getLogger(PurlResource.class);
+  @SuppressWarnings("unused")
+  private static final Logger logger = LogManager.getLogger(PurlResource.class);
 
-	@Context
-	private HttpServletRequest request;
+  @Context
+  private HttpServletRequest request;
 
-	@Context
-	private UriInfo uriInfo;
+  @Context
+  private UriInfo uriInfo;
 
-	/**
-	 * Show some welcome content at the root.
-	 * 
-	 * @return
-	 */
-	@GET
-	@Produces(MediaType.TEXT_HTML)
-	public String welcome() {
-		String html = StringUtil.fromInputStream(getClass().getResourceAsStream("welcome.html"));
-		String myBaseUrl = Registry.getInstance().getConfig().get("purl.baseurl");
-		if (myBaseUrl == null) {
-			myBaseUrl = "http://data.biodiversitydata.nl";
-		} else {
-			myBaseUrl = StringUtil.rtrim(myBaseUrl, '/');
-		}
-		html = html.replaceAll("@baseurl@", myBaseUrl);
-		return html;
-	}
+  /**
+   * Show some welcome content at the root.
+   * 
+   * @return
+   */
+  @GET
+  @Produces(MediaType.TEXT_HTML)
+  public String welcome() {
+    String html = StringUtil.fromInputStream(getClass().getResourceAsStream("welcome.html"));
+    String baseUrl = Registry.getInstance().getConfig().get("purl.baseurl");
+    if (baseUrl == null) {
+      baseUrl = "http://data.biodiversitydata.nl";
+    } else {
+      baseUrl = StringUtil.rtrim(baseUrl, '/');
+    }
+    html = html.replaceAll("${baseUrl}", baseUrl);
+    html = StringUtils.replaceOnce(html, "${version}", BuildInfo.getInstance().getVersion());
+    html = StringUtils.replaceOnce(html, "${buildDate}", BuildInfo.getInstance().getBuildDate());
+    html = StringUtils.replaceOnce(html, "${commitCount}", BuildInfo.getInstance().getCommitCount());
+    html = StringUtils.replaceOnce(html, "${gitBranch}", BuildInfo.getInstance().getGitBranch());
+    html = StringUtils.replaceOnce(html, "${gitCommit}", BuildInfo.getInstance().getGitCommit());
+    return html;
+  }
 
-	/**
-	 * Endpoint for Naturalis specimen PURLs.
-	 * 
-	 * @return
-	 */
-	@GET
-	@Path("/naturalis/specimen/{objectID}")
-	public Response handleNaturalisSpecimenPurl(@PathParam("objectID") String objectID) {
-		PurlHandler handler = new NaturalisPurlHandler(objectID, request, uriInfo);
-		return handler.handlePurl();
-	}
+  /**
+   * Endpoint for Naturalis specimen PURLs.
+   * 
+   * @return
+   */
+  @GET
+  @Path("/naturalis/specimen/{objectID}")
+  public Response handleNaturalisSpecimenPurl(@PathParam("objectID") String objectID) {
+    PurlHandler handler = new NaturalisPurlHandler(objectID, request, uriInfo);
+    return handler.handlePurl();
+  }
 
-	/**
-	 * Endpoint for Xeno-canto observation PURLs.
-	 * 
-	 * @return
-	 */
-	@GET
-	@Path("/xeno-canto/observation/{objectID}")
-	public Response handleXenoCantoSpecimenPurl(@PathParam("objectID") String objectID) {
-		XenoCantoPurlHandler handler = new XenoCantoPurlHandler(objectID, request, uriInfo);
-		return handler.handlePurl();
-	}
+  /**
+   * Endpoint for Xeno-canto observation PURLs.
+   * 
+   * @return
+   */
+  @GET
+  @Path("/xeno-canto/observation/{objectID}")
+  public Response handleXenoCantoSpecimenPurl(@PathParam("objectID") String objectID) {
+    XenoCantoPurlHandler handler = new XenoCantoPurlHandler(objectID, request, uriInfo);
+    return handler.handlePurl();
+  }
 
-	/**
-	 * Endpoint for Waarneming observation PURLs.
-	 * 
-	 * @return
-	 */
-	@GET
-	@Path("/obsint/observation/{objectID}")
-	public Response handleWaarnemingSpecimenPurl(@PathParam("objectID") String objectID) {
-		WaarnemingPurlHandler handler = new WaarnemingPurlHandler(objectID, request, uriInfo);
-		return handler.handlePurl();
-	}
+  /**
+   * Endpoint for Waarneming observation PURLs.
+   * 
+   * @return
+   */
+  @GET
+  @Path("/obsint/observation/{objectID}")
+  public Response handleWaarnemingSpecimenPurl(@PathParam("objectID") String objectID) {
+    WaarnemingPurlHandler handler = new WaarnemingPurlHandler(objectID, request, uriInfo);
+    return handler.handlePurl();
+  }
 
 }
